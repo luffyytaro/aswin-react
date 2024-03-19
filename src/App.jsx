646e50ec39f39dc01e57c24ee2a2aca3a855ebd9
aswin <aswin.cshajikumar@gmail.com>
2024-03-19 13:26:42 +0530

    SJKDFBLJbf

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { Component } from "react";
+
+class App extends Component{
+    constructor() {
+        super();
+        this.counter = 0;
+        this.state = {
+            counter: 0
+        };
+    }
+
+
+    abcd =() =>{
+        // console.log(this.counter);
+        this.setState({counter: this.state.counter+1})
+        this.counter++};
+
+
+    render() {
+        return(
+            <main>
+                {/* <Heading counter={this.counter}/> */}
+                <h2>{this.state.counter}</h2>
+                <button onClick={this.abcd}>inc</button>
+            </main>
+        );
+    }
+}
+export default App;
+
+class Heading extends Component {
+    constructor() {
+        super();
+    }
+
+    // render() {
+    //     return(
+    //         <h1>{this.props.counter}</h1>
+    //     );
+    // }
+} 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import  ReactDOM  from "react-dom/client";;
 import "./style.css";
+import App from "./App";
+
 
 const root = document.getElementById("root");
-ReactDOM.createRoot(root).render(
-    <main className="container">
-        <h1>Welcome to my Page</h1>
-        <input type="text" name="username"  placeholder="enter your username"/>
-        <input type="password" name="password" placeholder="enter your password" />
-        <input type="submit" value="submit" />
-    </main>
-)
+// ReactDOM.createRoot(root).render(<App/>);
+// ReactDOM.createRoot(root).render(
+//     <main className="container">
+//         <h1>Sign-up Page</h1>
+//         <input type="text" name="username"  placeholder="enter your username"/>
+//         <input type="password" name="password" placeholder="enter your password" />
+//         <input type="submit" value="submit" />
+//     </main>
+// )
