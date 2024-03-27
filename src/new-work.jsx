5c0511911052e0d3aa4b955adf3f69e84ba1d4f9
aswin <aswin.cshajikumar@gmail.com>
2024-03-27 13:09:07 +0530

    hakeem-sir

--- src/new-work.jsx
+++ src/new-work.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 
 
 
+
 function Dummy() {
-    
+
     const [apiData, setApiData] = useState({});
     useEffect(() => {
-        fetch("https://dummyjson.com/products/category/smartphones")
+        fetch("https://dummyjson.com/products/category/laptops")
             .then(res => res.json())
             .then(data => {
                 console.log("inside fetch");
@@ -15,16 +16,38 @@ function Dummy() {
     }, []);
     console.log("app");
     return (
+
+
         <main>
-            <div>{apiData?.products?.map((item, index) => (
+            <header>
+
+                <div className="headers">
+
+                    <div className="left-section">
+
+                        <a href="">Home</a>
+                        <a href="">About</a>
+                        <a href="">Contact Us</a>
+                    </div>
+
+                    <div className="right-section">
+                        <input type="text" placeholder="search" />
+                        <input type="submit" />
+                    </div>
+
+                </div>
+
+            </header>
+
+            <div className="content">{apiData?.products?.map((item, index) => (
                 <section key={index}>
                     <h1>{item.brand}</h1>
                     <img src={item.thumbnail} alt="" />
                     <h2>{item.title}</h2>
                     <h3>Price: ${item.price}</h3>
                 </section>
-
             ))}</div>
+
         </main>
     );
 }
