5bde4e72bb2d1a07d7267d48d2da478aec1ffa77
aswin <aswin.cshajikumar@gmail.com>
2024-04-09 01:39:20 +0530

    continue

--- sample-work/src/app.jsx
+++ sample-work/src/app.jsx
@@ -18,16 +18,16 @@ export default function App() {
             </nav>
 
             <div className="section1">
-              
-              <div className="sec2">
-              <div class="box">
-                    <img src="./icons8-duolingo-logo-500.png" alt="" width={150} />
+
+                <div className="sec2">
+                    <div class="box">
+                        <img src="./icons8-duolingo-logo-500.png" alt="" width={150} />
+                    </div>
+
+                    <span className="acc1"> <img src="./icons8-accreditation-60.png" alt="" height={80} width={80} /></span>
+                    <span className="acc2"><img src="./icons8-badge-96.png" alt="" height={80} width={80} /></span>
+
                 </div>
-
-                <span className="acc1"> <img src="./icons8-accreditation-60.png" alt="" height={80} width={80} /></span>
-                <span className="acc2"><img src="./icons8-badge-96.png" alt="" height={80} width={80} /></span>
-
-              </div>
                 <div className="button">
                     <button className="one"><img src="./icons8-login-24.png" alt="" />Corperate Login</button>
                     <button className="two"><img src="./icons8-download-24 (1).png" alt="" />Download Result</button>
@@ -36,10 +36,58 @@ export default function App() {
             </div>
 
             <div className="container">
-               {/* <img src="./main-banner.jpg"  alt="" /> */}
-               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores dolore ipsum nam voluptatem. Ipsam itaque nobis ipsum laudantium eligendi, aliquam doloribus veritatis inventore dolores dolor accusantium quisquam. Pariatur illum tempora ipsum corrupti nulla consectetur fugit obcaecati eligendi deleniti neque autem itaque possimus sunt, mollitia architecto molestias minus, nostrum consequuntur labore odio. Minima, perferendis eligendi? Quaerat, voluptate ab, delectus consectetur nostrum nihil, eius ullam perferendis distinctio ducimus tempora ut dolor neque nemo est fugiat velit recusandae. Unde incidunt ex veritatis quod animi molestias minima expedita amet quisquam maxime iusto tempora molestiae placeat quaerat cumque, id nisi odio quos suscipit vero error.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores dolore ipsum nam voluptatem. Ipsam itaque nobis ipsum laudantium eligendi, aliquam doloribus veritatis inventore dolores dolor accusantium quisquam. Pariatur illum tempora ipsum corrupti nulla consectetur fugit obcaecati eligendi deleniti neque autem itaque possimus sunt, mollitia architecto molestias minus, nostrum consequuntur labore odio. Minima, perferendis eligendi? Quaerat, voluptate ab, delectus consectetur nostrum nihil, eius ullam perferendis distinctio ducimus tempora ut dolor neque nemo est fugiat velit recusandae. Unde incidunt ex veritatis quod animi molestias minima expedita amet quisquam maxime iusto tempora molestiae placeat quaerat cumque, id nisi odio quos suscipit vero error.</p>
             </div>
 
+            <div className="skills">
+
+                <div className="micro">
+                    <img src="./icons8-vertical-line-100.png" alt="" width={30} height={90} />
+                    <img src="./microscope.png" alt="" height={60} width={60} />
+                    <p>Cutting-edge Technology</p>
+                    <img src="./icons8-vertical-line-100.png" alt="" width={30} height={90} />
+                </div>
+
+                <div className="heart">
+
+                    <img src="./heart-rate.png" alt="" height={60} width={60} />
+                    <p>Streamlined Cardiac Care</p>
+                    <img src="./icons8-vertical-line-100.png" alt="" width={30} height={90} />
+                </div>
+
+                <div className="help">
+
+                    <img src="./help.png" alt="" height={60} width={60} />
+                    <p>Rapid Results</p>
+
+                </div>
+
+                <div className="mri">
+                    <img src="./icons8-vertical-line-100.png" alt="" height={60} width={60} />
+                    <img src="./mri.png" alt="" height={90} width={90} />
+                    <p>Advanced Imaging</p>
+
+                </div>
+
+                <img src="./icons8-vertical-line-100.png" alt="" width={30} height={90} />
+                <div className="checklist">
+                    <img src="./checklist.png" alt="" height={60} width={60} />
+                    <p>Comprehensive Diagnostics</p>
+                    <img src="./icons8-vertical-line-100.png" alt="" width={30} height={90} />
+                </div>
+            </div>
+
+            <div className="content">
+
+                <div className="lab">
+                    <img src="./lab.jpg" alt="" />
+                </div>
+
+                <div className="text">
+                    <h3>Welcome to Excellence in Health Care</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam assumenda doloremque voluptas, a hic aspernatur ratione sit non, quia, sapiente voluptatibus. Praesentium eveniet alias consectetur ipsa illum quas mollitia iusto et ad in ut voluptate saepe, incidunt doloremque odit veniam possimus quam dicta, nulla porro deserunt sed eius voluptates maxime! Odit corporis corrupti amet voluptatum est laborum vel perferendis magnam repellat necessitatibus hic adipisci dignissimos atque exercitationem eius, iste reiciendis dolorem provident quod dolores quam eligendi praesentium? Animi, rerum voluptas. Cum assumenda quo error minus iure corrupti, saepe rerum nam incidunt, alias modi, vel ipsa nemo quidem necessitatibus culpa porro. Magnam sit recusandae sunt fugit hic provident illo voluptate ducimus? Fuga soluta adipisci quis quos deserunt inventore dicta hic pariatur quas necessitatibus quidem laudantium architecto optio nemo nesciunt, repellendus blanditiis velit labore et commodi doloribus omnis? Id unde delectus accusantium perferendis ea ipsam voluptate facilis excepturi libero optio ratione laudantium possimus, impedit, tempora earum animi, modi nobis quasi nihil? Eligendi ab voluptatem nesciunt repellendus quod iure earum impedit culpa minima voluptas. Impedit quaerat, est iste voluptatum omnis sequi ipsum neque rerum obcaecati? Dignissimos ipsam molestiae in eos molestias necessitatibus nesciunt magnam, harum consequuntur, aspernatur magni ut ipsum mollitia deleniti itaque commodi. Accusamus, sapiente delectus. Fugiat eligendi ipsam asperiores praesentium illum tempora at, deleniti unde temporibus quis? Accusamus labore reiciendis quas aperiam voluptates officiis dolores necessitatibus ducimus nostrum debitis totam fugiat nemo cupiditate ad quasi dolor, sit et doloribus distinctio quisquam iste aliquam eveniet minima. Aut nesciunt, in minus dignissimos quod omnis est blanditiis, sunt eveniet, quas fugit natus sit. Perferendis blanditiis dicta vero assumenda deserunt, ullam possimus iusto, tempora adipisci nihil expedita ea! Tempora quia reprehenderit non rerum delectus quos quod ea omnis id maiores saepe incidunt consequatur odit inventore, perspiciatis tempore vero facilis expedita commodi nesciunt excepturi repellendus temporibus?</p>
+                </div>
+            </div>
 
         </main>
     );
