6498bed39360013de8f6ef13686fd129575677a9
aswin <aswin.cshajikumar@gmail.com>
2024-04-19 13:18:33 +0530

    new

---
+++ Work1/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDom from "react-dom/client";
+import "./style.css";
+import Test from "./test";
+
+const root = document.getElementById("root");
+ReactDom.createRoot(root).render(<Test/>)
